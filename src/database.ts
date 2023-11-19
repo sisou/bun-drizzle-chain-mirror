523fd3b9e12605a438b4b2be8f204125cf825bad
Sören <hello@soerenschwert.de>
2023-11-19 14:10:49 -0600

    Limit database access to one connection without a pooler

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const url = process.env.DATABASE_POOLER_URL;
-if (!url) throw new Error("DATABASE_POOLER_URL environment variable is not set");
+const url = process.env.DATABASE_URL;
+if (!url) throw new Error("DATABASE_URL environment variable is not set");
 
 console.log("Opening PostgreSQL database");
-export const pg = postgres(url);
+export const pg = postgres(url, { max: 1 });
 export const db = drizzle(pg);
