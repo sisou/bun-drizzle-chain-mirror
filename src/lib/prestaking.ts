96b0f4930a62d8a9f9f54de8e39326792a973411
Sören <hello@soerenschwert.de>
2024-04-14 01:35:58 +0200

    Set actual migration block window heights

--- src/lib/prestaking.ts
+++ src/lib/prestaking.ts
@@ -3,13 +3,13 @@
  */
 export const VALIDATOR_DEPOSIT = 100_000e5;
 
-export const REGISTRATION_START_HEIGHT = 3_000_000; // TODO
-export const REGISTRATION_END_HEIGHT = 3_012_969; // TODO
+export const REGISTRATION_START_HEIGHT = 3016530;
+export const REGISTRATION_END_HEIGHT = 3022290;
 
 /**
  * Minimum amount of luna that must be delegated (staked).
  */
 export const MIN_DELEGATION = 100e5;
 
-export const PRESTAKING_START_HEIGHT = 3_012_970; // TODO
-export const PRESTAKING_END_HEIGHT = 4_000_000; // TODO
+export const PRESTAKING_START_HEIGHT = 3023730;
+export const PRESTAKING_END_HEIGHT = 3028050;
