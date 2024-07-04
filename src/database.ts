ec410d24ef4651f3a9e062313abd6042bd45701d
Sören <hello@soerenschwert.de>
2024-07-04 12:54:57 +0200

    Add schema to Drizzle client
    Oversight, ups.

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from "../db/schema";
 
 const url = process.env.DATABASE_URL;
 if (!url) throw new Error("DATABASE_URL environment variable is not set");
 
 console.log("Opening PostgreSQL database");
 export const pg = postgres(url, { max: 1 });
-export const db = drizzle(pg);
+export const db = drizzle(pg, { schema });
