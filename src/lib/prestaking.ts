cdd6e4ae1c6e843b145630b0a0955443398b8034
Sören <hello@soerenschwert.de>
2024-09-04 11:17:09 +0200

    Allow defining mainnet preregistration/prestaking block windows

--- src/lib/prestaking.ts
+++ src/lib/prestaking.ts
@@ -1,15 +1,18 @@
+const network = process.env.NETWORK || "testnet";
+const isMainnet = network.includes("main");
+
 /**
  * Validator deposit in luna.
  */
 export const VALIDATOR_DEPOSIT = 100_000e5;
 
-export const REGISTRATION_START_HEIGHT = 3016530;
-export const REGISTRATION_END_HEIGHT = 3022290;
+export const REGISTRATION_START_HEIGHT = isMainnet ? Infinity : 3016530;
+export const REGISTRATION_END_HEIGHT = isMainnet ? Infinity : 3022290;
 
 /**
  * Minimum amount of luna that must be delegated (staked).
  */
 export const MIN_DELEGATION = 100e5;
 
-export const PRESTAKING_START_HEIGHT = 3023730;
-export const PRESTAKING_END_HEIGHT = 3028050;
+export const PRESTAKING_START_HEIGHT = isMainnet ? Infinity : 3023730;
+export const PRESTAKING_END_HEIGHT = isMainnet ? Infinity : 3028050;
