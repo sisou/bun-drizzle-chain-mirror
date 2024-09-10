25b58bcedb6e955c04aa9ef20fcaf2738b326109
Sören <hello@soerenschwert.de>
2024-09-10 11:16:26 +0200

    Remove testnet prestaking windows
    In preparation for real testnet migration.

--- src/lib/prestaking.ts
+++ src/lib/prestaking.ts
@@ -6,13 +6,13 @@ const isMainnet = network.includes("main");
  */
 export const VALIDATOR_DEPOSIT = 100_000e5;
 
-export const REGISTRATION_START_HEIGHT = isMainnet ? Infinity : 3016530;
-export const REGISTRATION_END_HEIGHT = isMainnet ? Infinity : 3022290;
+export const REGISTRATION_START_HEIGHT = isMainnet ? Infinity : Infinity;
+export const REGISTRATION_END_HEIGHT = isMainnet ? Infinity : Infinity;
 
 /**
  * Minimum amount of luna that must be delegated (staked).
  */
 export const MIN_DELEGATION = 100e5;
 
-export const PRESTAKING_START_HEIGHT = isMainnet ? Infinity : 3023730;
-export const PRESTAKING_END_HEIGHT = isMainnet ? Infinity : 3028050;
+export const PRESTAKING_START_HEIGHT = isMainnet ? Infinity : Infinity;
+export const PRESTAKING_END_HEIGHT = isMainnet ? Infinity : Infinity;
