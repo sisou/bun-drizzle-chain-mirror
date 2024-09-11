c1ad759679f05aff3a7f6cf14b57b0d08424648d
Sören <hello@soerenschwert.de>
2024-09-11 17:13:09 +0200

    Set registration/prestaking window block numbers for mainnet

--- src/lib/prestaking.ts
+++ src/lib/prestaking.ts
@@ -8,13 +8,13 @@ export const BURN_ADDRESS = "NQ07 0000 0000 0000 0000 0000 0000 0000 0000";
  */
 export const VALIDATOR_DEPOSIT = 100_000e5;
 
-export const REGISTRATION_START_HEIGHT = isMainnet ? Infinity : Infinity;
-export const REGISTRATION_END_HEIGHT = isMainnet ? Infinity : Infinity;
+export const REGISTRATION_START_HEIGHT = isMainnet ? 3_357_600 : Infinity;
+export const REGISTRATION_END_HEIGHT = isMainnet ? 3_392_200 : Infinity;
 
 /**
  * Minimum amount of luna that must be delegated (staked).
  */
 export const MIN_DELEGATION = 100e5;
 
-export const PRESTAKING_START_HEIGHT = isMainnet ? Infinity : Infinity;
-export const PRESTAKING_END_HEIGHT = isMainnet ? Infinity : Infinity;
+export const PRESTAKING_START_HEIGHT = isMainnet ? 3_392_200 : Infinity;
+export const PRESTAKING_END_HEIGHT = isMainnet ? 3_456_000 : Infinity;
