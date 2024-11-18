8c185b1d05fa73d1ba402945a0621657574e7d36
Sören <hello@soerenschwert.de>
2024-11-18 00:02:26 -0600

    Set effective testnet pre-staking block windows and transition block.

--- src/lib/prestaking.ts
+++ src/lib/prestaking.ts
@@ -8,16 +8,18 @@ export const BURN_ADDRESS = "NQ07 0000 0000 0000 0000 0000 0000 0000 0000";
  */
 export const VALIDATOR_DEPOSIT = 100_000e5;
 
-export const REGISTRATION_START_HEIGHT = isMainnet ? 3_357_600 : Infinity;
-export const REGISTRATION_END_HEIGHT = isMainnet ? 3_392_200 : Infinity;
+export const REGISTRATION_START_HEIGHT = isMainnet ? 3_357_600 : 3_016_530;
+export const REGISTRATION_END_HEIGHT = isMainnet ? 3_392_200 : 3_022_290;
 
 /**
  * Minimum amount of luna that must be delegated (staked).
  */
 export const MIN_DELEGATION = 100e5;
 
-export const PRESTAKING_START_HEIGHT = isMainnet ? 3_392_200 : Infinity;
-export const PRESTAKING_END_HEIGHT = isMainnet ? 3_456_000 : Infinity;
+export const PRESTAKING_START_HEIGHT = isMainnet ? 3_392_200 : 3_023_730;
+export const PRESTAKING_END_HEIGHT = isMainnet ? 3_456_000 : 3_028_050;
+
+export const TRANSITION_BLOCK = isMainnet ? 3_456_000 : 3_032_010;
 
 export function getPoolAddressesAtBlockHeight(height: number) {
 	const poolAddresses = [
