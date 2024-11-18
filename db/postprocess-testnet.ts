e3d0a0e2fad617982ec8d04202cb5f452d6a9eb7
Sören <hello@soerenschwert.de>
2024-11-18 15:23:18 -0600

    Disable reverted-account postprocessing for testnet, now that it's done

--- db/postprocess-testnet.ts
+++ db/postprocess-testnet.ts
@@ -1,39 +1,39 @@
-import { eq, isNull, or } from "drizzle-orm";
-import { db } from "../src/database";
-import { getAccount as getPoSAccount } from "../src/pos/rpc";
+// import { eq, isNull, or } from "drizzle-orm";
+// import { db } from "../src/database";
+// import { getAccount as getPoSAccount } from "../src/pos/rpc";
 import TestnetGenesisAccounts from "./genesis-accounts.testnet.json" with { type: "json" };
 import { postprocess } from "./postprocess";
-import { accounts } from "./schema";
+// import { accounts } from "./schema";
 
 await postprocess(TestnetGenesisAccounts, async () => {
-	await updateRevertedAccountBalances();
+	// await updateRevertedAccountBalances();
 });
 
-async function updateRevertedAccountBalances() {
-	console.log("Updating reverted account balances...");
+// async function updateRevertedAccountBalances() {
+// 	console.log("Updating reverted account balances...");
 
-	const revertedAccounts = await db
-		.select({ address: accounts.address, balance: accounts.balance })
-		.from(accounts)
-		.where(or(
-			isNull(accounts.last_received),
-			isNull(accounts.last_sent),
-		));
+// 	const revertedAccounts = await db
+// 		.select({ address: accounts.address, balance: accounts.balance })
+// 		.from(accounts)
+// 		.where(or(
+// 			isNull(accounts.last_received),
+// 			isNull(accounts.last_sent),
+// 		));
 
-	console.log(`Found ${revertedAccounts.length} reverted accounts`);
+// 	console.log(`Found ${revertedAccounts.length} reverted accounts`);
 
-	let i = 1;
-	for (const { address, balance } of revertedAccounts) {
-		const account = await getPoSAccount(address);
+// 	let i = 1;
+// 	for (const { address, balance } of revertedAccounts) {
+// 		const account = await getPoSAccount(address);
 
-		if (account.balance !== balance) {
-			await db.update(accounts).set({ balance: account.balance }).where(eq(accounts.address, address));
-			console.log(
-				`${i}/${revertedAccounts.length} Updated balance of ${address} from ${balance} to ${account.balance}`,
-			);
-		}
-		i++;
-	}
+// 		if (account.balance !== balance) {
+// 			await db.update(accounts).set({ balance: account.balance }).where(eq(accounts.address, address));
+// 			console.log(
+// 				`${i}/${revertedAccounts.length} Updated balance of ${address} from ${balance} to ${account.balance}`,
+// 			);
+// 		}
+// 		i++;
+// 	}
 
-	console.log("Done updating reverted account balances");
-}
+// 	console.log("Done updating reverted account balances");
+// }
