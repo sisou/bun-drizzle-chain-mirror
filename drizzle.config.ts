f44107c0696c6ec3c303a2a4f136aa89c3c0d007
Sören <hello@soerenschwert.de>
2025-08-28 14:32:14 +0200

    Let Drizzle write migrations table into public schema
    So that it gets replicated.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,9 @@ export default {
 		// biome-ignore lint/style/noNonNullAssertion: we know this is set
 		url: process.env.DATABASE_URL!,
 	},
+	migrations: {
+		schema: 'public',
+	},
 	verbose: true,
 	strict: true,
 } satisfies Config;
